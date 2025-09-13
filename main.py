3339703e3484578381f8d1fde4e415f1c9df3fac
Conor <220956160+conor-sm@users.noreply.github.com>
2025-09-13 14:54:25 +0100

    Ball movement

--- main.py
+++ main.py
@@ -44,11 +44,50 @@ class Player:
         pygame.draw.rect(screen, (255, 100, 255), self.paddle_rect)
 
 class Ball:
-    def __init__(self):
+    def __init__(self, x, y, radius, speed_x, speed_y):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+
+    def move(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+    def bounce(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+        if self.y - self.radius <= 0 or self.y + self.radius >= SCREEN_HEIGHT:
+            self.speed_y *= -1
+        if self.x - self.radius <= 0 or self.y + self.radius >= SCREEN_WIDTH:
+            self.speed_x *= -1
+
+    def check_out_bounds(self, SCREEN_WIDTH):
+        return self.x - self.radius <= 0 or self.x + self.radius >= SCREEN_WIDTH
+
+    def reset(self, SCREEN_WIDTH, SCREEN_HEIGHT ):
+        self.x = SCREEN_WIDTH // 2
+        self.y = SCREEN_HEIGHT // 2
+        self.speed_x = random.choice([-5, 5])
+        self.speed_y = random.choice([-5, 5])
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 100, 255), (self.x, self.y), self.radius)
 
 class AI:
     def __init__(self):
         self.text = "Hello World"
+
+player = Player()
+ai = AI()
+ball = Ball(400, 225, 10, 5, 5)
+
+def game():
+    player.draw(screen)
+    player.update()
+    ball.move()
+    ball.bounce(WINDOW_WIDTH, WINDOW_HEIGHT)
+    if ball.check_out_bounds(WINDOW_WIDTH):
+        ball.reset(WINDOW_WIDTH, WINDOW_HEIGHT)
+    ball.draw(screen)
 
 def menu():
     screen.fill((0, 0, 0))
@@ -64,8 +103,6 @@ def game_over_screen():
     screen.blit(line_one_B, ((WINDOW_WIDTH - line_one_B.get_width()) // 2, ((WINDOW_HEIGHT - line_one_B.get_height()) // 2)))
     screen.blit(line_two_B, ((WINDOW_WIDTH - line_two_B.get_width()) // 2, ((WINDOW_HEIGHT - line_two_B.get_height()) // 2 + 50)))
 
-player = Player()
-ai = AI()
 while running:
 
     screen.fill((0, 0, 0))
@@ -92,8 +129,7 @@ while running:
         game_over()
 
     if game_active:
-        player.draw(screen)
-        player.update()
+        game()
 
     clock.tick(60)
     pygame.display.update()
